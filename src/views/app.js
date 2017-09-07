6e6e5ce3360906939e9a5bbf8375d31d9f70411a
Clara Cho <clara.cho@gmail.com>
2017-09-06 20:35:12 -0700

    pass all spec using mocked data

--- src/views/app.js
+++ src/views/app.js
@@ -3,7 +3,10 @@ var AppView = Backbone.View.extend({
   el: '#app',
 
   initialize: function() {
-    this.videos = new Videos();
+    this.videos = new Videos(window.exampleVideoData);
+    this.render();
+    var videoPlayer = new VideoPlayerView();
+    var videoList = new VideoListView({ collection: this.videos });
   },
 
 
--- src/views/videoList.js
+++ src/views/videoList.js
@@ -1,10 +1,20 @@
 var VideoListView = Backbone.View.extend({
 
+  initialize: function() {
+    this.collection.forEach(this.renderVideo, this);
+    this.collection.on('sync', this.render, this);
+    this.render();
+  },
 
   render: function() {
     this.$el.children().detach();
     this.$el.html(this.template());
     return this;
+  },
+
+  renderVideo: function(video) {
+    var videoView = new VideoListEntryView({ model: video });
+    this.$el.append(videoView.render());
   },
 
   template: templateURL('src/templates/videoList.html')
